e5fe39e1bd28bb280a30e5a50022e9af906a88b2
Flutter-Code-Craft <muneefullah24mdswe363@gmail.com>
2026-09-22 11:52:55 -0700

    add all files

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,33 @@ import 'package:study_mate/Screens/Splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Study Mate',
+      title: 'StudyMate',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF196EEE), // Matches StudyMate primary blue
+          primary: const Color(0xFF196EEE),
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
       ),
-        home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
-
